564599cbf8118f2bfd67478afe4ccc572dc7e548
Williams Bolade <dbolade2000@gmail.com>
2023-01-04 16:29:40 +0100

    reverse proxy to test

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
+using ReverseProxyApplication;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseMiddleware<ReverseProxyMiddleware>();
 
-app.Run();
+// app.MapGet("/", () =>
+// {
+//     return "Hello World!";
+// }).RequireAuthorization();
+app.Run(async (context) =>
+ {
+     await context.Response.WriteAsync("<a href='/googleforms/d/e/1FAIpQLSdJwmxHIl_OCh-CI1J68G1EVSr9hKaYFLh3dHh8TLnxjxCJWw/viewform?hl=en'>Register to receive a T-shirt</a>");
+ });
